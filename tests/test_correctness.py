bb10f84a82e6326e221ebef2dca243af0da0a805
osick <oliver.sick@web.de>
2024-08-06 19:31:57 +0200

    test script fix

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -6,7 +6,7 @@ def test_correctness():
     with open("testdata.json","r") as fh: testdata=json.load(fh)
     for pos in testdata:
         nodes = perft(pos["fen"], pos["depth"])
-        print(f"{'  ERROR: ' if nodes != pos['nodes'] else 'SUCCESS: '}depth={pos['depth']:<2} Expected={pos['nodes']:<8} Result={nodes:<8} remark:{pos['remark'][:17]+('...' if len(pos['remark'])>15 else''):<21} {pos['fen']=:<15}")
+        print(f"{'  ERROR: ' if nodes != pos['nodes'] else 'SUCCESS: '}depth={pos['depth']:<2} Expected={pos['nodes']:<8} Result={nodes:<8} remark:{pos['remark'][:17]+('...' if len(pos['remark'])>15 else''):<21} pos['fen']={pos['fen']:<15}")
 
 if __name__ =="__main__":
     test_correctness()
