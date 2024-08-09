1f56501937e9e8feeb3dc6afd78f0b4c4a217173
osick <oliver.sick@web.de>
2024-08-09 14:48:06 +0200

    test update 4

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -1,15 +1,18 @@
 import json
 import os
+import sys
 
 from pycmg import perft
 
 def test_correctness():
+    verbose = (len(sys.argv)>=2 and sys.argv[1]=="verbose")
+
     success= True
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "testdata.json"),"r") as fh: testdata=json.load(fh)
     for pos in testdata:
         nodes = perft(pos["fen"], pos["depth"])
         success = success and (nodes == pos['nodes'])
-        #print(f"{'  ERROR: ' if nodes != pos['nodes'] else 'SUCCESS: '}depth={pos['depth']:<2} Expected={pos['nodes']:<8} Result={nodes:<8} remark:{pos['remark'][:17]+('...' if len(pos['remark'])>15 else''):<21} pos['fen']={pos['fen']:<15}")
+        if verbose: print(f"{'  ERROR: ' if nodes != pos['nodes'] else 'SUCCESS: '}depth={pos['depth']:<2} Expected={pos['nodes']:<8} Result={nodes:<8} remark:{pos['remark'][:17]+('...' if len(pos['remark'])>15 else''):<21} pos['fen']={pos['fen']:<15}")
     return success
 if __name__ =="__main__":
     print("\nTest Case ",__file__,sep="")
--- tests/test_set_position.py
+++ tests/test_set_position.py
@@ -19,25 +19,26 @@ def test_fen(depth=3):
         fen_total+=c_fen_result
     return fen_total
 
-def test_correctness(depth=3):
+def test_correctness(depth=3,verbose=False):
     inp_nodes , fen_nodes = 0 , 0
 
     start = time(); 
     inp_nodes=test_inp(depth); 
     t=time()-start; 
-    print(f"inp: Time:{t:.2f}, NPS={int(round(inp_nodes/t,0)):_}")
+    if verbose: print(f"inp: Time:{t:.2f}, NPS={int(round(inp_nodes/t,0)):_}, {inp_nodes=}")
 
     start = time(); 
     fen_nodes=test_fen(depth); 
     t=time()-start; 
-    print(f"fen: Time:{t:.2f}, NPS={int(round(fen_nodes/t,0)):_}")
+    if verbose: print(f"fen: Time:{t:.2f}, NPS={int(round(fen_nodes/t,0)):_}, {fen_nodes=}")
     
     return inp_nodes, fen_nodes
 
 if __name__ =="__main__":
+    verbose = (len(sys.argv)>=2 and sys.argv[1]=="verbose")
     print("\nTest Case ",__file__,sep="")
     depth = 6 if len(sys.argv)<2 else (6 if (not sys.argv[1].isdigit()) else int(sys.argv[1]))
-    inp_res, fen_res = test_correctness(depth)
+    inp_res, fen_res = test_correctness(depth, verbose)
     success = (inp_res == fen_res)
 
-    {print ("TEST PASSED", inp_res, fen_res) and exit(0)} if success else {print ("TEST FAILED", inp_res, fen_res) and exit(1)}
+    {print ("TEST PASSED") and exit(0)} if success else {print ("TEST FAILED") and exit(1)}
