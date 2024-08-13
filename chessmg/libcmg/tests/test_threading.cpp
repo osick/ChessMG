e98a509a60522da6777be849e28d45a308892916
osick <oliver.sick@web.de>
2024-08-13 23:01:08 +0200

    Threading improvements

--- chessmg/libcmg/tests/test_threading.cpp
+++ chessmg/libcmg/tests/test_threading.cpp
@@ -11,26 +11,16 @@
 #include "libsurge.h"
 #include <locale>
 
-
-template<Color Us> 
-unsigned long long dist_perft(Position& p, unsigned int maxdepth) {
-	unsigned long long nodes = 0;
-	MoveList<Us> list(p);
-	for (Move move : list) {
-        p.play<Us>(move);
-        nodes += perft_start<~Us>(p.fen(), maxdepth - 1);
-        p.undo<Us>(move);
-    }
-    return nodes;
-};
-
 template<Color Us> 
 unsigned long long perft_start(std::string fen, unsigned int depth) {
+        std::cout <<"fen=" << fen <<" depth=" <<depth << " started" << std::endl; 
+        std::cout.flush();
         long long result = 0; 
         Position next_pos; 
         Position::set(fen,next_pos);
-    	MoveList<Us> list(next_pos);
         result = perft<Us>(next_pos, depth);
+        std::cout <<"fen=" << fen <<" depth=" <<depth << " DONE! Nodes=" <<result << std::endl; 
+        std::cout.flush();
         return result;
 }
 
@@ -48,10 +38,8 @@ unsigned long long perft(Position& p, unsigned int depth) {
 };
 
 int main(int argc,char* argv[]){
-    int maxdepth=atoi(argv[1]);
-    
+    int maxdepth = argc>=2 ? atoi(argv[1]) : 6;
     std::cout.imbue(std::locale(""));
-
     initialise_all_databases();
 	zobrist::initialise_zobrist_keys();	
     Position p;
@@ -61,9 +49,8 @@ int main(int argc,char* argv[]){
     std::vector<std::future<unsigned long long>> futures {};
     std::chrono::steady_clock::time_point begin {};
     std::chrono::steady_clock::time_point end {};
-
-
 	begin = std::chrono::steady_clock::now();
+
     for (Move move : list) {
         p.play<WHITE>(move);
         futures.emplace_back(std::async(perft_start<BLACK>,p.fen(), maxdepth-1) );
@@ -76,7 +63,7 @@ int main(int argc,char* argv[]){
     auto diff = end - begin;
     int count = std::chrono::duration_cast<std::chrono::microseconds>(diff).count();
     std::cout << " Nodes=" << nodes <<std::endl;
-    std::cout << " Duration=" << count/1000 << " Milliseconds" << std::endl;
-    std::cout << " NPS=" << nodes / (count / 1'000'000) << std::endl;
+    std::cout << " Duration=" << count << " Mikroseconds" << std::endl;
+    std::cout << " NPS=" << nodes / count * 1'000'000 << std::endl;
     return 0;
 };
